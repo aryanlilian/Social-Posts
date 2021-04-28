c4108a805bf01812696d9674ef63fd4d9d3744d7
Bularu Lilian <aryanlilian@gmail.com>
2021-04-28 13:34:48 +0200

    configured database tables indexes on more fields

--- posts/models.py
+++ posts/models.py
@@ -21,7 +21,7 @@ class Author(AbstractUser):
     first_name = models.CharField(_('First Name'), max_length=100, null=False, blank=False)
     last_name = models.CharField(_('Last Name'), max_length=100, null=False, blank=False)
     # the models.ManyToManyField represents a Many-to-Many relationship between the Author and the List model
-    lists = models.ManyToManyField(List)
+    lists = models.ManyToManyField(List, db_index=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} - {self.username}'
@@ -42,7 +42,7 @@ class Post(models.Model):
     title = models.CharField(_('Title'), max_length=250, null=False, blank=False, db_index=True)
     content = models.TextField(_('Post content'), null=False, blank=False, db_index=True)
     social_network = models.CharField(
-        _('Social Network'), max_length=8, choices=SocialNetwork.choices, default=SocialNetwork.FACEBOOK
+        _('Social Network'), max_length=8, choices=SocialNetwork.choices, default=SocialNetwork.FACEBOOK, db_index=True
     )
     link = models.URLField(_('Link'), max_length=200)
     created_date = models.DateTimeField(_('Created Date/Time'), auto_now_add=True, db_index=True)
